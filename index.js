3de2884ce2d4ce5103faedee2e640b0fe21a943c
Baodoan1812 <doanngocbaoltn@gmail.com>
2024-09-17 21:06:56 +0700

    them __dirname

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ app.use(cookieParser('keyboard cat'));
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 
-app.use(express.static('public'))
+app.use(express.static(`${__dirname}/public`))
 const route=require("./routes/client/index.route")
 const routeAdmin =require("./routes/admin/index.route")
-app.set('views', './views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 routeAdmin(app);
 route(app)
